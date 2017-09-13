c69031092a411cccd1b3980a5ad55130b236d7a8
jenifer2015 <31485128+jenifer2015@users.noreply.github.com>
2017-09-13 13:24:26 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np 
-import pylab as pl 
-pl.title('Jenifer')
-x =  [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-Y =  [1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
-pl.xlabel('Edad')
-pl.ylabel('anio')
-pl.plot(x,Y)
-pl.savefig('p0.png')
+import matplotlib.pylab as plt 
+def f(y):
+    return 2* np.cos(y) + 2015
+y1 = np.arange(0.0,5.0,0.1)
+plt.plot(y1, f(y1),'bo', y1, f(y1), 'g--')
+plt.savefig('p1.png')
+
+plt.show()
+  
